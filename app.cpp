ef42ab2f30b678ca1b1a9926df1e5250658f9a51
Hulkenn <kevin.kaunisvaara@gmail.com>
2020-04-15 15:21:07 +0200

    init commit

---
+++ app.cpp
@@ -0,0 +1,18 @@
+#include <iostream.h>
+class Hello {
+ public:
+ Hello(){
+ std::cout << "Hello World!";
+ }
+};
+int main(){
+ Hello * h = new Hello();
+ delete h;
+ return 0;
+}
+class Kevin{
+    public:
+    Kevin(){
+        
+    }
+}
